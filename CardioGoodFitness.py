3cf507cc72ced1c39e4d51f76475be0dfff90946
j-oshi <jtoshin@msn.com>
2024-11-13 17:08:19 +0000

    upate

---
+++ CardioGoodFitness.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+st.write("""
+    # My first app
+    Hello *world!*
+    """)
+
+# df = pd.read_csv("CardioGoodFitness.csv")
+# df_copy = df.copy()
+# st.line_chart(df_copy)
